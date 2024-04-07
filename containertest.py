e4c493dfc227f6e36e02d4d3885f1e6f420a6a62
Anwesha Das <anwesha@das.community>
2024-04-07 16:07:57 +0200

    Adds image type detection

--- containertest.py
+++ containertest.py
@@ -1,45 +1,61 @@
+#!/usr/bin/env python
+
 from podman import PodmanClient
 import os
+import sys
 
-uri = "unix:///run/user/1000/podman/podman.sock"
-dir_name = os.path.dirname(os.path.realpath(__file__))
+def main():
+    if len(sys.argv) < 2:
+        print("Please provide the image id to test.")
+        sys.exit(-1)
+    image_id = sys.argv[1]
+    uri = "unix:///run/user/1000/podman/podman.sock"
+    dir_name = os.path.dirname(os.path.realpath(__file__))
 
-volume = [
-    {
-        "type": "bind",
-        "source": f"{dir_name}/alltests.py",
-        "target": "/runner/alltests.py",
-        "relabel": "Z",
-    },
-    {
-        "type": "bind",
-        "source": f"{dir_name}/vars.json",
-        "target": "/runner/vars.json",
-        "relabel": "Z",
-    },
-]
+    volume = [
+        {
+            "type": "bind",
+            "source": f"{dir_name}/alltests.py",
+            "target": "/runner/alltests.py",
+            "relabel": "Z",
+        },
+        {
+            "type": "bind",
+            "source": f"{dir_name}/vars.json",
+            "target": "/runner/vars.json",
+            "relabel": "Z",
+        },
+    ]
 
-with PodmanClient(base_url=uri) as client:
-    # find if any container exists  then delete
-    containers = client.containers.list(all=True, filters={"name": "testrun"})
-    if len(containers) == 1:
-        print("Found running conatainer, stopping and removing it.")
-        c = containers[0]
-        c.stop(ignore=True, timeout=1)
-        c.remove()
+    with PodmanClient(base_url=uri) as client:
+        # find if any container exists  then delete
+        containers = client.containers.list(all=True, filters={"name": "testrun"})
+        if len(containers) == 1:
+            print("Found running conatainer, stopping and removing it.")
+            c = containers[0]
+            c.stop(ignore=True, timeout=1)
+            c.remove()
 
-    image = client.images.get("2cf8285eaa56")
-    print(f"Starting container image {image.tags[0]}.")
-    container = client.containers.run(
-        image,
-        "/usr/bin/bash",
-        name="testrun",
-        detach=True,
-        tty=True,
-        mounts=volume,
-        remove=True,
-        environment={"IMAGENAME": "base"},
-    )
-    code, output = container.exec_run("/usr/bin/python3 /runner/alltests.py")
+        image = client.images.get(image_id)
+        if image_id.find('minimal') != -1:
+            type_of_image = 'minimal'
+        else:
+            type_of_image = 'base'
+        print(f"Starting container image {image.tags[0]}.")
+        container = client.containers.run(
+            image,
+            "/usr/bin/bash",
+            name="testrun",
+            detach=True,
+            tty=True,
+            mounts=volume,
+            remove=True,
+            environment={"IMAGENAME": type_of_image},
+        )
+        code, output = container.exec_run("/usr/bin/python3 /runner/alltests.py")
 
-    print(output.decode("utf-8", errors="ignore"))
+        print(output.decode("utf-8", errors="ignore"))
+
+if __name__ == "__main__":
+    main()
+
