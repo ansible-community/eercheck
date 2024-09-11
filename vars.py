71bf5c2ef7245b7d422c3a36d3540861080f8402
Anwesha Das <anwesha@das.community>
2024-09-11 21:23:59 +0200

    Updates core and collection versions for 2.17.4-1 releases

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
 import json
 
-vars = {"ansible-core-version":"2.16.1", "fedora-version": "39","ansible.posix": "1.5.4", "ansible.utils": "2.11.0", "ansible.windows": "2.1.0"}
+vars = {"ansible-core-version":"2.17.4", "fedora-version": ",40","ansible.posix": "1.5.4", "ansible.utils": "4.1.0", "ansible.windows": "2.5.0"}
 
 with open("vars.json","w") as fobj:
     data = json.dumps(vars, indent=4)
