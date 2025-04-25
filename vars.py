92f9fbc1aa783d38b7a83e8101b9256b556a46b2
Anwesha Das <anwesha@das.community>
2025-04-25 11:09:32 +0200

    Edits the ansible-core vesion in vars.py

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
 import json
 
-vars = {"ansible-core-version":"2.17.4", "fedora-version": ",40","ansible.posix": "1.5.4", "ansible.utils": "4.1.0", "ansible.windows": "2.5.0"}
+vars = {"ansible-core-version":"2.18.5", "fedora-version": ",40","ansible.posix": "1.6.2", "ansible.utils": "5.1.0", "ansible.windows": "2.8.0"}
 
 with open("vars.json","w") as fobj:
     data = json.dumps(vars, indent=4)
