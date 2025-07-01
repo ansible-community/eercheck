180bc0d0de24560891bbee48638dd9ad19914bc4
Anwesha Das <anwesha@das.community>
2025-07-01 20:44:58 +0200

    Added correct versions and cleans repo

--- vars.py
+++
@@ -1,7 +0,0 @@
-import json
-
-vars = {"ansible-core-version":"2.18.6", "fedora-version": ",42","ansible.posix": "1.6.2", "ansible.utils": "5.1.2", "ansible.windows": "2.8.0"}
-
-with open("vars.json","w") as fobj:
-    data = json.dumps(vars, indent=4)
-    fobj.write(data)
